a31770f87435d17fc141da8be863640ed12c4278
Richard <richard9372@gmail.com>
2016-08-15 14:58:46 +0800

    Comment unused code.

--- src/Wukong/Services/ChannelManager.cs
+++ src/Wukong/Services/ChannelManager.cs
@@ -2,30 +2,30 @@ using System.Collections.Generic;
 
 namespace Wukong.Services
 {
-    interface IChannelManager
-    {
-        
-    }
-    public class ChannelManager : IChannelManager
-    {
-        private ISocketManager socketManager;
-        private IProvider provider;
-        public ChannelManager(ISocketManager socketManager, IProvider provider)
-        {
-            this.socketManager = socketManager;
-            this.provider = provider;
-        }
-        public void Join(string channelId, string userId)
-        {
-            var channel = Storage.Instance.GetChannel(channelId) ?? 
-                Storage.Instance.CreateChannel(channelId, socketManager, provider);
-            if (channel.UserList.Contains(userId)) {
-            }
-       }
-
-       public void BroadCastUserList(Channel channel, string userId = null)
-       {
-           var userList = userId != null ? new List<string> { userId } : channel.UserList;
-       }
-    }
+//    interface IChannelManager
+//    {
+//        
+//    }
+//    public class ChannelManager : IChannelManager
+//    {
+//        private ISocketManager socketManager;
+//        private IProvider provider;
+//        public ChannelManager(ISocketManager socketManager, IProvider provider)
+//        {
+//            this.socketManager = socketManager;
+//            this.provider = provider;
+//        }
+//        public void Join(string channelId, string userId)
+//        {
+//            var channel = Storage.Instance.GetChannel(channelId) ?? 
+//                Storage.Instance.CreateChannel(channelId, socketManager, provider);
+//            if (channel.UserList.Contains(userId)) {
+//            }
+//       }
+//
+//       public void BroadCastUserList(Channel channel, string userId = null)
+//       {
+//           var userList = userId != null ? new List<string> { userId } : channel.UserList;
+//       }
+//    }
 }
