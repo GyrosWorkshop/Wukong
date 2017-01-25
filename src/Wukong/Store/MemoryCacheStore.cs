d375c8e6a4eccb781f4ec31016dbaadb17b4ac5a
Richard <richard9372@gmail.com>
2017-01-25 14:55:23 +0800

    WIP: fix #29 (#30)
    * save authentication details inside session (server memory)
    
    * fix: MemoryCacheStore not found
    
    * Fix: startup reference wrong type

---
+++ src/Wukong/Store/MemoryCacheStore.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wukong.Store
+{
+    public class MemoryCacheStore : ITicketStore
+    {
+        private const string KeyPrefix = "AuthSessionStore";
+        private readonly IMemoryCache _cache;
+
+        public MemoryCacheStore()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+        }
+
+        public async Task<string> StoreAsync(AuthenticationTicket ticket)
+        {
+            var key = KeyPrefix + Guid.NewGuid();
+            await RenewAsync(key, ticket);
+            return key;
+        }
+
+        public Task RenewAsync(string key, AuthenticationTicket ticket)
+        {
+            var options = new MemoryCacheEntryOptions();
+            var expiresUtc = ticket.Properties.ExpiresUtc;
+
+            if (expiresUtc.HasValue)
+            {
+                options.SetAbsoluteExpiration(expiresUtc.Value);
+            }
+
+            options.SetSlidingExpiration(TimeSpan.FromDays(30));
+
+            _cache.Set(key, ticket, options);
+
+            return Task.FromResult(0);
+        }
+
+        public Task<AuthenticationTicket> RetrieveAsync(string key)
+        {
+            AuthenticationTicket ticket;
+            _cache.TryGetValue(key, out ticket);
+            return Task.FromResult(ticket);
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            _cache.Remove(key);
+            return Task.FromResult(0);
+        }
+    }
+}
