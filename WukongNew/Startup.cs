0c9f4ba17086252a62cbdafba4417fe3b3e42eda
Senorsen <senorsen.zhang@gmail.com>
2017-03-13 01:53:59 +0800

    order...

--- WukongNew/Startup.cs
+++ WukongNew/Startup.cs
@@ -96,10 +96,10 @@ namespace Wukong
             app.UseCors(builder => builder.WithOrigins("http://127.0.0.1:8080", "http://localhost:8080").AllowAnyMethod().AllowAnyHeader().AllowCredentials());
             app.UseCookieAuthentication(Options.AuthenticationOptions.CookieAuthenticationOptions());
 
-            app.UseGoogleAuthentication(OAuthProviderOptions.GoogleOAuthOptions(Configuration["Authentication:Google:ClientId"],
-                Configuration["Authentication:Google:ClientSecret"]));
             app.UseOAuthAuthentication(OAuthProviderOptions.GitHubOAuthOptions(Configuration["Authentication:GitHub:ClientId"],
                 Configuration["Authentication:GitHub:ClientSecret"]));
+            app.UseGoogleAuthentication(OAuthProviderOptions.GoogleOAuthOptions(Configuration["Authentication:Google:ClientId"],
+                Configuration["Authentication:Google:ClientSecret"]));
 
             app.UseWebSockets();
             app.UseMiddleware<SocketManagerMiddleware>();
