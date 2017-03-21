406def14e679ce94e403744ae6cf5e9d0f559ba3
Leeleo3x <leeleo3x@gmail.com>
2017-03-21 15:28:14 -0700

    Rename appsetting files.

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 set -e
 
 echo "Use wukong-provider: ${WUKONG_PROVIDER}"
-cp appsettings.template.json appsettings.json
-sed -i "s/\"APPLICATION_INSIGHTS_INSTRUMENTATION_KEY\"/\"${APPLICATION_INSIGHTS_INSTRUMENTATION_KEY}\"/" appsettings.json
-sed -i "s/\"GOOGLE_CLIENT_ID\"/\"${GOOGLE_CLIENT_ID}\"/" appsettings.json
-sed -i "s/\"GOOGLE_CLIENT_SECRET\"/\"${GOOGLE_CLIENT_SECRET}\"/" appsettings.json
-sed -i "s/\"GITHUB_CLIENT_ID\"/\"${GITHUB_CLIENT_ID}\"/" appsettings.json
-sed -i "s/\"GITHUB_CLIENT_SECRET\"/\"${GITHUB_CLIENT_SECRET}\"/" appsettings.json
-sed -i "s/\"MICROSOFT_CLIENT_ID\"/\"${MICROSOFT_CLIENT_ID}\"/" appsettings.json
-sed -i "s/\"MICROSOFT_CLIENT_SECRET\"/\"${MICROSOFT_CLIENT_SECRET}\"/" appsettings.json
+cp appsettings.template.json appsettings.Production.json
+sed -i "s/\"APPLICATION_INSIGHTS_INSTRUMENTATION_KEY\"/\"${APPLICATION_INSIGHTS_INSTRUMENTATION_KEY}\"/" appsettings.Production.json
+sed -i "s/\"GOOGLE_CLIENT_ID\"/\"${GOOGLE_CLIENT_ID}\"/" appsettings.Production.json
+sed -i "s/\"GOOGLE_CLIENT_SECRET\"/\"${GOOGLE_CLIENT_SECRET}\"/" appsettings.Production.json
+sed -i "s/\"GITHUB_CLIENT_ID\"/\"${GITHUB_CLIENT_ID}\"/" appsettings.Production.json
+sed -i "s/\"GITHUB_CLIENT_SECRET\"/\"${GITHUB_CLIENT_SECRET}\"/" appsettings.Production.json
+sed -i "s/\"MICROSOFT_CLIENT_ID\"/\"${MICROSOFT_CLIENT_ID}\"/" appsettings.Production.json
+sed -i "s/\"MICROSOFT_CLIENT_SECRET\"/\"${MICROSOFT_CLIENT_SECRET}\"/" appsettings.Production.json
 WUKONG_PROVIDER_ESCAPE=${WUKONG_PROVIDER//\//\\\/}
-sed -i "s/\"WUKONG_PROVIDER\"/\"${WUKONG_PROVIDER_ESCAPE}\"/" appsettings.json
+sed -i "s/\"WUKONG_PROVIDER\"/\"${WUKONG_PROVIDER_ESCAPE}\"/" appsettings.Production.json
 dotnet run -p WukongNew/Wukong.csproj -c RELEASE
