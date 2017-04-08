a5df4215de9c0fcdecd5d98af742c28c68c1decb
Senorsen <senorsen.zhang@gmail.com>
2017-04-08 20:59:22 +0800

    Update Dockerfile.build and start.sh and csproj

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ sed -i "s/\"MICROSOFT_CLIENT_SECRET\"/\"${MICROSOFT_CLIENT_SECRET}\"/" appsettin
 WUKONG_PROVIDER_ESCAPE=${WUKONG_PROVIDER//\//\\\/}
 sed -i "s/\"WUKONG_PROVIDER\"/\"${WUKONG_PROVIDER_ESCAPE}\"/" appsettings.Production.json
 mv appsettings.Production.json WukongNew/appsettings.Production.json
-./dist/Wukong
+./wukong-dist/Wukong
