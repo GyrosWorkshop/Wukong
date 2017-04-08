26838565d856d77568b99523fccd0ac9c206e64a
Senorsen <senorsen.zhang@gmail.com>
2017-04-08 21:08:28 +0800

    linux-x64 runtime identifier

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ sed -i "s/\"MICROSOFT_CLIENT_SECRET\"/\"${MICROSOFT_CLIENT_SECRET}\"/" appsettin
 WUKONG_PROVIDER_ESCAPE=${WUKONG_PROVIDER//\//\\\/}
 sed -i "s/\"WUKONG_PROVIDER\"/\"${WUKONG_PROVIDER_ESCAPE}\"/" appsettings.Production.json
 mv appsettings.Production.json WukongNew/appsettings.Production.json
-./wukong-dist/Wukong
+../wukong-dist_linux-x64/Wukong
