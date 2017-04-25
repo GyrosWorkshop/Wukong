e56aa138acb3373375a1533f46d692d167ae2da1
Leeleo3x <leeleo3x@gmail.com>
2017-04-24 18:32:18 -0700

    Refine code.

--- Wukong/Startup.cs
+++ Wukong/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
