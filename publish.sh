d2efe129a8b35ac3cc62cde8f7b4549c294cb2c0
Senorsen <senorsen.zhang@gmail.com>
2017-09-30 22:18:21 +0800

    Remove redundant

--- publish.sh
+++ publish.sh
@@ -6,6 +6,6 @@ OUTPUT_PREFIX=Wukong
 
 for rid in $*; do
     OUTPUT="$OUTPUT_PREFIX-$rid"
-    dotnet publish $CSPROJ -c Release -o $OUTPUT -r $rid
+    dotnet publish $CSPROJ -c Release -o ../$OUTPUT -r $rid
     tar czvf $OUTPUT.tar.gz $OUTPUT
 done
