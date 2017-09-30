a47b362270451097ae85dfb99abf6e5c21b8a007
Senorsen <senorsen.zhang@gmail.com>
2017-09-30 20:19:19 +0800

    Add travis build release artifacts to github
    Fix travis ci config typo
    
    Fix dotnet version
    
    Fix script
    
    Add dotnet publish
    
    Fix path
    
    Add --self-contained parameter.
    
    Fix travis ci release (#76)
    
    * Fix sdk?
    
    * Add ubuntu.14.04-x64
    
    * Add to tests
    
    Add publish.sh, multiple portable executables
    
    Fix travis ci build
    
    Fix restore
    
    Fix build
    
    Remove redundant
    
    Fix build using docker

---
+++ publish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+CSPROJ=Wukong/Wukong.csproj
+CONFIGURATION=Release
+OUTPUT_PREFIX=Wukong
+
+for rid in $*; do
+    OUTPUT="$OUTPUT_PREFIX-$rid"
+    dotnet publish $CSPROJ -c Release -o ../$OUTPUT -r $rid
+    tar czvf $OUTPUT.tar.gz $OUTPUT
+done
