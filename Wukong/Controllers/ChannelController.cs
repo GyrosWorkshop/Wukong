704a8978893d457ea19d813e652c6758b5018f35
Richard <richard9372@gmail.com>
2018-10-31 21:46:08 +0800

    Fix: npe

--- Wukong/Controllers/ChannelController.cs
+++ Wukong/Controllers/ChannelController.cs
@@ -34,7 +34,11 @@ namespace Wukong.Controllers
         public async Task<IActionResult> GetCurrent(string channelId)
         {
             var channel = storage.GetChannel(channelId);
-            return new ObjectResult(await channel.GetCurrent());
+            if (channel == null) {
+                return NoContent();
+            } else {
+                return new ObjectResult(await channel.GetCurrent());
+            }
         }
 
         [HttpPost("finished")]
